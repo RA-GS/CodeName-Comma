673c96361749a33b5ae260943efb2574dd50c7da
RahulM-3 <rahulcode97@gmail.com>
2024-01-10 21:01:43 +0530

    test world

--- Assets/Player ped/player.cs
+++ Assets/Player ped/player.cs
@@ -1,35 +1,34 @@
-using system;
-
-namespace palyersped;
-
-class playerAswin
+namespace palyersped
 {
-    string name = "aswin";
-    string ability = "Fast worker";
-    private int speed = 5;
-    private int health = 100;
-}
+    class playerAswin
+    {
+        string name = "aswin";
+        string ability = "Fast worker";
+        private int speed = 5;
+        private int health = 100;
+    }
 
-class playerGuna
-{
-    string name = "guna";
-    string ability = "Strong";
-    private int speed = 3;
-    private int health = 130;
-}
+    class playerGuna
+    {
+        string name = "guna";
+        string ability = "Strong";
+        private int speed = 3;
+        private int health = 130;
+    }
 
-class playerRahul
-{
-    string name = "rahul";
-    string ability = "Eagle eye";
-    private int speed = 4;
-    private int health = 100
-}
+    class playerRahul
+    {
+        string name = "rahul";
+        string ability = "Eagle eye";
+        private int speed = 4;
+        private int health = 100;
+    }
 
-class playerSriram
-{
-    string name = "sriram";
-    string ability = "Fast worker";
-    private int speed = 5;
-    private int health = 100;
+    class playerSriram
+    {
+        string name = "sriram";
+        string ability = "Fast worker";
+        private int speed = 5;
+        private int health = 100;
+    }
 }
